96de757f8b6d4b91524f8e3bd6052e37e4eea5c4
Erlimar Silva Campos <erlimar@gmail.com>
2016-07-30 20:06:20 -0300

    Added TypeScript support

---
+++ _builder/tasks/app-tasks/tsc.js
@@ -0,0 +1,30 @@
+// Copyright (c) E5R Development Team. All rights reserved.
+// Licensed under the Apache License, Version 2.0.
+// More license information in LICENSE.txt.
+
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    ts = require('gulp-typescript'),
+    sm = require('gulp-sourcemaps'),
+    fs = require('fs');
+
+gulp.task('tsc', [], function () {
+    var tsconfigData = JSON.parse(fs.readFileSync('tsconfig.json')),
+        tsconfig = ts.createProject('tsconfig.json'),
+        project = ts(tsconfig),
+        destPath = (((tsconfigData || {}).compilerOptions || {}).rootDir) || 'src',
+        sourceMap = !!((((tsconfigData || {}).compilerOptions || {}).sourceMap) || false),
+        tsResult = tsconfig.src();
+
+    if (sourceMap) {
+        tsResult = tsResult.pipe(sm.init());
+    }
+
+    tsResult = tsResult.pipe(project).js;
+
+    if (sourceMap) {
+        tsResult = tsResult.pipe(sm.write('./'));
+    }
+
+    return tsResult.pipe(gulp.dest(destPath));
+});
---
+++ _builder/tasks/watch.js
@@ -0,0 +1,21 @@
+// Copyright (c) E5R Development Team. All rights reserved.
+// Licensed under the Apache License, Version 2.0.
+// More license information in https://github.com/e5r/licenses/blob/master/notices/empty-project.rst.
+
+/** @todo: Move to e5r/empty-project */
+
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+
+    log = gutil.log,
+    pairs = [
+        { glob: 'src/**/*.ts', tasks: ['tsc'] }
+    ];
+
+gulp.task('watch', [], function () {
+    for (var p in pairs) {
+        var pair = pairs[p];
+        gulp.watch(pair.glob, pair.tasks);
+        log('Watching "' + pair.glob + '"...');
+    }
+});
