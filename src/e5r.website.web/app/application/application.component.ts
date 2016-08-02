cc941b7ffc12df438a6d340cd7772c7fd503b019
Erlimar Silva Campos <erlimar.campos@basis.com.br>
2016-08-02 19:45:21 -0300

    Added Angular support

---
+++ src/e5r.website.web/app/application/application.component.ts
@@ -0,0 +1,14 @@
+// Copyright (c) E5R Development Team. All rights reserved.
+// Licensed under the Apache License, Version 2.0. More license information in LICENSE.txt.
+
+/// <reference path="../../../../typings/index.d.ts" />
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'application',
+    template: `
+    <h1>E5R Development Team</h1>
+    `
+})
+export class ApplicationComponent { }
--- src/e5r.website.web/app/main.ts
+++ src/e5r.website.web/app/main.ts
@@ -1,2 +1,10 @@
 // Copyright (c) E5R Development Team. All rights reserved.
 // Licensed under the Apache License, Version 2.0. More license information in LICENSE.txt.
+
+/// <reference path="../../../typings/index.d.ts" />
+
+import {bootstrap} from '@angular/platform-browser-dynamic';
+
+import {ApplicationComponent} from './application/application.component';
+
+bootstrap(ApplicationComponent);
