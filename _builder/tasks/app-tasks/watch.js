0b42c10c83ea3da36ade067b7d589b83055207bb
Erlimar Silva Campos <erlimar.campos@basis.com.br>
2016-08-05 17:37:32 -0300

    Bugfix on watch Gulp Task,

--- _builder/tasks/app-tasks/watch.js
+++ _builder/tasks/app-tasks/watch.js
@@ -1,21 +1,14 @@
 // Copyright (c) E5R Development Team. All rights reserved.
 // Licensed under the Apache License, Version 2.0. More license information in LICENSE.txt.
 
-var gulp = require('gulp'),
-    gutil = require('gulp-util'),
+var gulp = require('gulp');
 
-    log = gutil.log,
-    pairs = [
-        { glob: 'src/**/*.ts', tasks: ['tsc'] },
-        { glob: 'src/**/*.scss', tasks: ['sass'] }
-    ];
+gulp.task('watch', [], function() {
+    gulp.watch('src/**/*.ts', function(event) {
+        gulp.start('tsc');
+    });
 
-gulp.task('watch', [], function () {
-    for (var p in pairs) {
-        var pair = pairs[p];
-        gulp.watch(pair.glob, function (event) {
-            gulp.start(pair.tasks);
-        });
-        log('Watching "' + pair.glob + '"...');
-    }
+    gulp.watch('src/**/*.scss', function(event) {
+        gulp.start('sass');
+    });
 });
